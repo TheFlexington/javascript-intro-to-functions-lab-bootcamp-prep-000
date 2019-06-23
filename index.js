3938e5a47db1804b3b89641aaf52a873851a6c69
Felix Martinez <felixaviermartinez@gmail.com>
2019-06-23 12:41:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,21 +17,11 @@ function logWhisper(string) {
   console.log("hello")
 }
 
-function sayHiToGrandma(string) {
-  if (logShout(string))
-  return "YES INDEED!"
+function sayHiToGrandma (string) {
+ if (logWhisper)
+ return ('I can\'t hear you!');
+ if (logShout)
+ return ('YES INDEED!');
+ if ('I love you, Grandma.')
+ return ('I love you, too.');
 }
-
-function sayHiToGrandma(string) {
-  if (logWhisper(string))
-  return "I can't hear you!"
-}
-
-function sayHiToGrandma(string) {
-  if (string === "I love you, Grandma.")
-  return "I love you, too."
-}
-
-sayHiToGrandma("hello");
-sayHiToGrandma("HELLO");
-sayHiToGrandma("I love you, Grandma.");
