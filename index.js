5a61b71c0bfaba5b24e8be8cc9a9d326076514ec
Felix Martinez <felixaviermartinez@gmail.com>
2019-06-23 12:21:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,15 +19,15 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) 
-    return 'I can\'t hear you!.';
+    return 'I can\'t hear you!.'
 }
 function sayHiToGrandma(string) {
   if (string === "I love you, Grandma.")
-    return "I love you, too.";
+    return "I love you, too."
 }
 function sayHiToGrandma(string) {
   if (string === string.toUpperCase()) 
-    return "YES INDEED!";
+    return "YES INDEED!"
 }
 
 sayHiToGrandma("hello");
